beec07ee3154e71dacd64589f131d1f4e5355644
abu bokor suraj <abusurajbd@gmail.com>
2022-11-24 10:52:21 +0600

    Fixed installation errors

---
+++ src/Pages/Routes/Routes/Routes.js
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../../Layout/Main";
+import ErrorPage from "../../ErrorPage/ErrorPage";
+import Home from "../../Home/Home/Home";
+
+export const routes = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        errorElement:<ErrorPage></ErrorPage>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/home',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
