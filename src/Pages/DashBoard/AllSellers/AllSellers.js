e4ee119782b9520e8b88bf3766ded22c051f3682
abu bokor suraj <abusurajbd@gmail.com>
2022-11-26 22:45:04 +0600

    myProduct page implemented

--- src/Pages/DashBoard/AllSellers/AllSellers.js
+++ src/Pages/DashBoard/AllSellers/AllSellers.js
@@ -1,85 +1,90 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import Swal from 'sweetalert2';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import Swal from "sweetalert2";
 
 const AllSellers = () => {
+  const {
+    data: sellers = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["sellers"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/sellers");
+      const data = await res.json();
+      return data;
+    },
+  });
 
-    const { data: sellers = [], refetch, isLoading } = useQuery({
-        queryKey: ['sellers'],
-        queryFn: async () => {
-            const res = await fetch('http://localhost:5000/sellers');
-            const data = await res.json();
-            return data
-        }
-    });
- 
-    const handleDelete = (id) =>{
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              fetch(`http://localhost:5000/seller/${id}`,{
-                method: 'DELETE'
-            })
-            .then( res =>res.json())
-            .then(data =>{
-              /// deletedCount // dont forget this spelling
-              if(data?.deletedCount > 0){
-                // const remaining = sellers?.filter(mrvw => mrvw._id !== id);
-                // setMyReview(remaining);
-                refetch()
-                 
-              }
-            })
-              Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-              )
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/seller/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            /// deletedCount // dont forget this spelling
+            if (data?.deletedCount > 0) {
+              refetch();
             }
-          })
-        }
-
-    // console.log(sellers);
-    if(isLoading){
-        return  <div className=" my-5 mx-auto w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div> 
-    }
-    return (
-<div>
-<h2 className="text-3xl my-5 text-center">Total sellers: {sellers.length}</h2>
-            <div className="overflow-x-auto">
-  <table className="table w-full">
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        sellers?.map((seller, i) =><tr key={seller._id}>
-            <th>{i+1}</th>
-            <td>{seller.name}</td>
-            <td>{seller.email}</td>
-            {/* <td>{ user?.role !== 'admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-xs btn-primary'>Make Admin</button>}</td> */}
-            <td><button onClick={() => handleDelete(seller._id)}  className='btn btn-xs btn-error'>Delete</button></td>
-          </tr>)
+          });
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
       }
-      
-    </tbody>
-  </table>
+    });
+  };
 
-</div>
-        </div>
+  // console.log(sellers);
+  if (isLoading) {
+    return (
+      <div className=" my-5 mx-auto w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
     );
+  }
+  return (
+    <div className="mx-4">
+      <h2 className="text-3xl my-5 text-center">
+        Total sellers: {sellers?.length}
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sellers?.map((seller, i) => (
+              <tr key={seller._id}>
+                <th>{i + 1}</th>
+                <td>{seller.name}</td>
+                <td>{seller.email}</td>
+                {/* <td>{ user?.role !== 'admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-xs btn-primary'>Make Admin</button>}</td> */}
+                <td>
+                  <button
+                    onClick={() => handleDelete(seller._id)}
+                    className="btn btn-xs btn-error"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllSellers;
--- src/Pages/DashBoard/MyProducts/MyProducts.js
+++ src/Pages/DashBoard/MyProducts/MyProducts.js
@@ -1,11 +1,80 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../Context/AuthProvider";
 
 const MyProducts = () => {
+  const { user } = useContext(AuthContext);
+  const email = user?.email;
+  console.log(email);
+  const {
+    data: myProducts = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["myproducts"],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/myproducts/seller/${email}`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+  if (isLoading) {
     return (
-        <div>
-            <h2>my products</h2>
-        </div>
+      <div className=" my-5 mx-auto w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
     );
+  }
+  console.log(myProducts);
+  return (
+    <div className="mx-4">
+      <h2 className="text-3xl my-5 text-center">
+        Total Products: {myProducts?.length}
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Action</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myProducts?.map((myProduct, i) => (
+              <tr key={myProduct._id}>
+                <th>{i + 1}</th>
+                <td>{myProduct.productName}</td>
+                <td>{myProduct.resalePrice}</td>
+                {/* <td>{ user?.role !== 'admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-xs btn-primary'>Make Admin</button>}</td> */}
+
+                <td>
+                  <select className="select select-primary max-w-xs">
+                    <option >
+                      Available
+                    </option>
+                    <option>Sold</option>
+                     
+                    
+                  </select>
+                </td>
+                <td>
+                  <button
+                    // onClick={() => handleDelete(seller._id)}
+                    className="btn btn-xs btn-error"
+                  >
+                    Advertise
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyProducts;
