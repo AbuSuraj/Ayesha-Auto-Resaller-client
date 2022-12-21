1a4c8897a7590190bab1adfb87a06e1a7556b8e5
Abu Bokor Suraj <abusurajbd@gmail.com>
2022-12-22 01:31:39 +0600

    made navbar manu active

--- src/Pages/SharedPages/Header/Header.js
+++ src/Pages/SharedPages/Header/Header.js
@@ -1,29 +1,50 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider';
 
 const Header = () => {
     const {user, logOut} = useContext(AuthContext);
+    let activeStyle = {
+        backgroundColor: 'BFEAF5',
+        // color: 'black',
+        paddingLeft: '4px',
+        paddingRight: '4px',
+        paddingTop: '2px',
+        paddingBottom: '2px',
+        borderRadius: '10px'
+      };
     const handleLogOut = () => {
         logOut()
             .then(() => { })
             .catch(err => console.log(err));
     }
     const menuItems = <React.Fragment>
-  <li><Link to="/">Home</Link></li>
-  <li><Link to="/blog">Blog</Link></li>
+  <li><NavLink style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }   to="/">Home</NavLink></li>
+  <li><NavLink style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }  to="/blog">Blog</NavLink></li>
   {/* <li><Link to="/login">Sign In</Link></li> */}
   
   {user?.uid ?
       <>
-          <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link> {user?.displayName}</Link></li>
+          <li><NavLink style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }  to="/dashboard">Dashboard</NavLink></li>
+          <li><NavLink style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            } > {user?.displayName}</NavLink></li>
           <li><button onClick={handleLogOut}>Sign out</button></li>
       </>
       : 
       <>
-      <li><Link to="/login">Sign In</Link></li>
-      <li><Link to="/register">Sign Up</Link></li>
+      <li><NavLink style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            } to="/login">Sign In</NavLink></li>
+      <li><NavLink style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }  to="/register">Sign Up</NavLink></li>
       </>
       
       }
@@ -35,9 +56,9 @@ const Header = () => {
                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <nav tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                     {menuItems}
-                </ul>
+                </nav>
             </div>
             <img
                   className="rounded-full"
