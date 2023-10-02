66aecd4162e2bb26c3c7a12f5e09e2ea85f5c833
Sk. Sanjidul Haque <sanjidul@xpertsolvers.com>
2023-10-02 13:34:39 +0600

    remove

--- src/Pages/Form/Login/Login.js
+++ src/Pages/Form/Login/Login.js
@@ -49,8 +49,8 @@ const Login = () => {
           navigate(from, { replace: true });
         });
       };
-      
-    //   console.log(loginUserEmail);
+
+
 
       if(loading){
         return  <div className=" my-5 mx-auto w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div> 
